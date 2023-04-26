b199707e50517ed8cb6d08c40c24be4b04108a1b
Olegm1212 <128962445+Olegm1212@users.noreply.github.com>
2023-04-26 13:32:02 +0300

    task 13 HW

---
+++ task13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.Write("введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+string GetThirdDigit(int r)
+{
+    string b = Convert.ToString(r);
+    Console.WriteLine($"{b[2]}");
+    return b;
+}
+if (a>=100)
+{
+    GetThirdDigit(a);
+}
+else
+{
+    Console.Write("Третьей цифры нет");
+}
