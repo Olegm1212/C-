081df4bc16480e0949a0ae9a122653109754294d
Olegm1212 <128962445+Olegm1212@users.noreply.github.com>
2023-04-26 13:31:48 +0300

    task 10 HW

---
+++ task10/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int GetThreeDigitNumber()
+{
+    int rand = new Random().Next(100, 1000);
+    Console.WriteLine(rand);
+    return rand;
+}
+
+string GetSecondDigit(int r)
+{
+    string b = Convert.ToString(r);
+    Console.WriteLine($"{b[1]}");
+    return b;
+}
+
+int rand = GetThreeDigitNumber();
+
+GetSecondDigit(rand);
