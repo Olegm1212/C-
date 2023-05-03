cb385574f7f53e82d27482201864f488964faafb
Olegm1212 <128962445+Olegm1212@users.noreply.github.com>
2023-05-03 17:51:05 +0300

    add task19

---
+++ task19/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+
+int GetFiveDigitNumber()
+{
+    Console.Write("Please, enter a 5 digit number: ");
+    int rand = int.Parse(Console.ReadLine());
+    return rand;
+    
+}
+
+
+void CheckPalindrom(int r)
+{
+int n, i;
+int sum = 0;
+//Console.Write(+r);
+for ( i = r; i > 0 ; i = i/10 )
+{
+    n = i%10;
+    sum = sum*10 + n;
+}
+Console.Write(+sum);
+if (r == sum)
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write("No");
+}
+}
+
+
+int rand = GetFiveDigitNumber();
+if (rand < 9999 || rand > 100000)
+    {
+         Console.Write("It is not a 5 digit number" + rand);
+    }
+else
+    {
+        CheckPalindrom(rand);   
+    }
+
+
+
+
+
+
