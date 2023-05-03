cd2e0b878a3febc13e95c83f72b94307bcde3a5a
Olegm1212 <128962445+Olegm1212@users.noreply.github.com>
2023-05-03 17:53:45 +0300

    add task23

---
+++ task23/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+
+int n = Promt("Введите число: ");
+ if (n==0)
+    Console.Write("0");
+else
+CalcCube(n);
+
+int Promt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return Math.Abs(a);
+}
+
+void CalcCube(int n)
+{
+    int i = 1;
+    int cube;
+    while(i<=n)
+{
+        Console.WriteLine (cube = i*i*i);
+        i++;
+}
+}
