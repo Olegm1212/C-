21f2414333247bbe9e70e078bcc024117675bf8a
Olegm1212 <128962445+Olegm1212@users.noreply.github.com>
2023-05-22 11:49:36 +0300

    Sum of Digitals

---
+++ task27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return Math.Abs(a);
+}
+
+void DigitsSum(int n)
+{
+    int digsum=0, i;
+    for (i=n; i>0; i=i/10)
+    {
+        digsum=digsum+i%10;
+    }
+    Console.WriteLine (digsum);
+}
+
+int n = Prompt("Введите число: ");
+DigitsSum(n);
