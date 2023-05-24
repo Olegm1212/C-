49dd8da1de0f8af1ff5f18aab929b7cb2ef6455d
Olegm1212 <128962445+Olegm1212@users.noreply.github.com>
2023-05-24 10:25:54 +0300

    Even Sum

---
+++ task34/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] a = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        a[i] = new Random().Next(minValue, maxValue+1);
+        Console.Write($"{a[i]} ");
+
+    }
+    return a;
+}
+
+void EvenNum(int[] arr)
+{
+    int num=0;
+    foreach(int el in arr)
+    {
+        if(el%2==0)
+        {
+            num++;
+        }
+    }
+    Console.Write($"Number of even numbers = {num}");
+}
+
+
+int[] array = GetArray(4, 100, 999);
+EvenNum(array);
