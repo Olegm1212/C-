9509f691ef05856038e76b269fd86dabfff30e87
Olegm1212 <128962445+Olegm1212@users.noreply.github.com>
2023-05-25 10:35:02 +0300

    Difference between Max and Min Elements

---
+++ task38/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+
+
+double[] GetArray(int size)
+{
+    double[] a = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        a[i] = new Random().NextDouble();
+        Console.Write($"{a[i]} ");
+    }
+    return (a);
+}
+
+void DifferenceMaxMin(double[] arr)
+{
+    int i = 0;
+    double max = arr[i], min = arr[i+1], dif=0;
+    foreach(double el in arr)
+    {
+
+        if(el < min)
+        {
+            min = el;
+        }
+        else if(el > max)
+        {
+            max = el;
+        }
+    }
+    Console.WriteLine();
+    Console.Write($" Difference between Max and Min = {max-min}");
+}
+
+
+double[] array = GetArray(4);
+DifferenceMaxMin(array);
